1e0b1689951291c6dd8a1f8dd380981a0f2e5306
TheUnknownMonkey <miha.puc@gmail.com>
2022-12-06 20:40:24 +0100

    compleate game

--- script.js
+++ script.js
@@ -11,36 +11,80 @@ const scissors = document.getElementById("scissors");
 let playerScore = 0;
 let computerScore = 0;
 const choices = ["Rock", "Paper", "Scissors"];
-let playerCoice ;
+let playerCoice =0;
+let ch = [rock, paper, scissors];
+
+function singleGame (playerCoice){
+  let computeChoice = Math.floor(Math.random()*3);
+  ch[computeChoice].style.border="10px solid red";
+    if (playerCoice==computeChoice){
+        ch[computeChoice].style.border="10px solid purple";
+        document.getElementById("announcer").innerHTML = "Tie, you both picked "+choices[playerCoice];
+    }
+    else if(playerCoice==0 && computeChoice==1||playerCoice==1 && computeChoice==2||playerCoice==2 && computeChoice==0 ){
+
+      
+      computerScore++;
+      document.getElementById("computer-score").innerHTML = ""+computerScore;
+      document.getElementById("announcer").innerHTML = "You loose becouse you picked "+choices[playerCoice]+" and the computer picked "+choices[computeChoice];
+
+      if (computerScore==5){
+        document.getElementById("announcer").innerHTML = "Computer wins with :"+computerScore+" to: "+playerScore+" click any item to play again!";
+        playerScore=0;
+        computerScore=0;
+        document.getElementById("computer-score").innerHTML = ""+computerScore;
+        document.getElementById("player-score").innerHTML = ""+playerScore;
+      }
+    }
+    else{
+      
+      playerScore++;
+
+      document.getElementById("player-score").innerHTML = ""+playerScore;
+      document.getElementById("announcer").innerHTML = "You win becouse you picked "+choices[playerCoice]+" and the computer picked "+choices[computeChoice];
+      if (playerScore==5){
+        document.getElementById("announcer").innerHTML = "You win "+playerScore+" to "+computerScore+" click any item to play again!";
+        playerScore=0;
+        computerScore=0;
+        document.getElementById("computer-score").innerHTML = ""+computerScore;
+        document.getElementById("player-score").innerHTML = ""+playerScore;
+      }
+
+    }
+    
+
+}
 
 function rockClicked() {
+    reset();
     playerCoice=0;
+    singleGame(playerCoice)
+    rock.style.border="10px solid blue"
   }
 function paperClicked() {
- 
+    reset();
     playerCoice=1;
+    singleGame(playerCoice)
+    paper.style.border="10px solid blue"
   }
 function scissorsClicked() {
-  
+    reset();
     playerCoice=2;
+    singleGame(playerCoice)
+    scissors.style.border="10px solid blue"
   }
   
+function reset(){
+  paper.style.border="6px solid black";
+  scissors.style.border="6px solid black";
+  rock.style.border="6px solid black";
+}
 
 rock.addEventListener("click", rockClicked);
 paper.addEventListener("click", paperClicked);
 scissors.addEventListener("click", scissorsClicked);
 
 //     //let userChoice =parseInt(prompt("pick rock paper or scissors, by typing 1, 2 or 3"));
-let computeChoice = Math.floor(Math.random()*3);
 
-function playerSelection (playerCoice){
 
-}
 
-function singleGame (playerCoice, computeChoice){
-    if (playerCoice==computeChoice){
-        alert("same pick!")
-    }
-
-}
-singleGame(playerCoice,computeChoice)
